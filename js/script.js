44cc7297d5afe0a1a77f53ad9a84801090d3f300
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-17 10:57:59 -0400

    Almost finishing the main page

--- js/script.js
+++ js/script.js
@@ -8,5 +8,14 @@ function toggleVideo(){
 function changeBg(bg, title) {
   const banner = document.querySelector('.banner');
 const contents = document.querySelectorAll('.content');
-banner.style.background = 'url("../ima'
+banner.style.background = 'url("../images/${bg}")';
+banner.style.backgroundsize = 'cover';
+banner.style.backgroundPosition = 'center';
+
+contents.forEach(content => {
+  content.classList.remove('active');
+  if (content.classList.contains(title)) {
+    content.classList.add('active');
+  }
+});
 }
