09cc167bf5517383ea124acddaece17a17eedcc8
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-17 11:40:36 -0400

    Done with the Main Page!!!!!

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ function toggleVideo(){
   trailer.classList.toggle('active')
 }
 
-function changeBg(bg, title) {
-  const banner = document.querySelector('.banner');
-const contents = document.querySelectorAll('.content');
-banner.style.background = 'url("../images/${bg}")';
-banner.style.backgroundsize = 'cover';
-banner.style.backgroundPosition = 'center';
+// function changeBg(bg, title) {
+//   const banner = document.querySelector('.banner');
+// const contents = document.querySelectorAll('.content');
+// banner.style.background = 'url("./images/${bg}")';
+// banner.style.backgroundsize = 'cover';
+// banner.style.backgroundPosition = 'center';
 
-contents.forEach(content => {
-  content.classList.remove('active');
-  if (content.classList.contains(title)) {
-    content.classList.add('active');
-  }
-});
-}
+// contents.forEach(content => {
+//   content.classList.remove('active');
+//   if (content.classList.contains(title)) {
+//     content.classList.add('active');
+//   }
+// });
+// }
