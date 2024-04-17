438d07078e2970071ce5a0aaac00a564971ba787
AbelZewdie <Abelmzewdie@gmail.com>
2024-04-17 10:20:31 -0400

    Working on the JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function toggleVideo(){
+  const trailer = document.querySelector('.trailer')
+  const video = document.querySelector('video')
+  video.pause();
+  trailer.classList.toggle('active')
+}
+
+function changeBg(bg, title) {
+  const banner = document.querySelector('.banner');
+const contents = document.querySelectorAll('.content');
+banner.style.background = 'url("../ima'
+}
